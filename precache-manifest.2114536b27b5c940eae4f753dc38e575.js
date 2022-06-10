1f518c3eeeb268ed84b2269ed2f31a19732cdb32
marutech <106373689+marutech@users.noreply.github.com>
2022-06-10 13:05:19 +0800

    Add files via upload

---
+++ precache-manifest.2114536b27b5c940eae4f753dc38e575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645202e4b6ac15d2fcb3779dd7b31a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c25238eda29daa7682",
+    "url": "/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "8b482ef48aab0e74d807",
+    "url": "/static/js/2.9d5a57f7.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.9d5a57f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c25238eda29daa7682",
+    "url": "/static/js/main.e2415159.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e2415159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a05b06fd665074fd25",
+    "url": "/static/js/runtime-main.ec7ff6d9.js"
+  }
+]);
